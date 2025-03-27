3ee40a4c7c4f4220734ac082a81bf9fc792839bf
VaghesanSundaram <vaghesan@gmail.com>
2025-03-26 20:19:37 -0400

    .

---
+++ create_profile.py
@@ -0,0 +1,8 @@
+import pynput
+import time
+
+keyphrase = "the quick brown fox jumps over the lazy dog"
+
+print("This will create a profile based on how you type the phrase, \"" + keyphrase + "\"")
+print("Start typing whenever")
+
